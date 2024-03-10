5c40ce364d29f8353ecd455de473b4c06f6e205e
Adam Thorpe <ajthor@users.noreply.github.com>
2024-03-10 12:25:20 -0500

    Updating pendulum example.

--- cem.py
+++ cem.py
@@ -21,10 +21,11 @@ class CEMParams:
 
 
 def sample(key, n: int, params: CEMParams):
-    """Sample from the multivariate normal distribution defined by the mean and std.
+    """Sample action sequences from the multivariate normal distribution.
 
     args:
         key: PRNG key
+        n: Number of sequences to generate.
         params: CEMParams
 
     returns:
@@ -47,13 +48,38 @@ def sample(key, n: int, params: CEMParams):
     return samples
 
 
-def select_elites(n: int, samples: jnp.ndarray, scores: jnp.ndarray):
+def update_params(elites: jnp.ndarray, params: CEMParams, theta: float = 1):
+    """Update the parameters of the CEM.
+
+    args:
+        elites: The top n elites samples.
+        params: CEMParams
+        theta: The mixing parameter between 0 and 1, where 0 means no update and 1 means
+            full update.
+
+    returns:
+        CEMParams
+
+    """
+
+    if theta < 1:
+        mean = jnp.mean(elites, axis=0) * theta + params.mean * (1 - theta)
+        std = jnp.std(elites, axis=0) * theta + params.std * (1 - theta)
+
+    else:
+        mean = jnp.mean(elites, axis=0)
+        std = jnp.std(elites, axis=0)
+
+    return CEMParams(mean=mean, std=std, elites=elites)
+
+
+def select_elites(samples: jnp.ndarray, scores: jnp.ndarray, n: int = 10):
     """Select the top n elites from the samples based on the scores.
 
     args:
-        n: Number of elites to select.
         samples: Samples to be selected from.
         scores: Scores of the samples.
+        n: Number of elites to select.
 
     returns:
         Tuple of the top n elites samples and their scores.
@@ -61,39 +87,39 @@ def select_elites(n: int, samples: jnp.ndarray, scores: jnp.ndarray):
     """
 
     # Get the indices of the top n_elites samples
-    elite_indices = jnp.argsort(scores)[-n:]
+    # NOTE: jnp.argsort sorts by increasing order, so we select the first n indices.
+    elite_indices = jnp.argsort(scores)[:n]
 
     return samples[elite_indices], scores[elite_indices]
 
 
-def update_params(elites: jnp.ndarray, params: CEMParams):
-    """Update the parameters of the CEM.
+def shift_elites(key, elites: jnp.ndarray, params: CEMParams):
+    """Shift the elites to remove the first action and append a new action.
 
     args:
+        key: PRNG key
         elites: The top n elites samples.
         params: CEMParams
 
     returns:
-        CEMParams
+        The shifted elites.
 
     """
 
-    mean = jnp.mean(elites.flatten(), axis=0)
-    std = jnp.std(elites.flatten(), axis=0)
+    samples = (
+        _generate_colored_sample(
+            key,
+            1,
+            (elites.shape[0], elites.shape[2], elites.shape[1]),
+        ).transpose((0, 2, 1))
+        * params.std
+        + params.mean
+    )
 
-    return CEMParams(mean, std, elites)
+    # Append the new actions to the elites
+    new_actions = samples[:, -1, :]
+    shifted_elites = jnp.concatenate(
+        (elites[:, 1:, :], new_actions[:, None, :]), axis=1
+    )
 
-
-# Test
-rng = jax.random.PRNGKey(0)
-mean = jnp.zeros((10, 3))
-std = jnp.ones((10, 3))
-elites = jnp.empty_like(mean)
-params = CEMParams(mean=mean, std=std, elites=elites)
-
-n = 100
-
-samples = sample(rng, n=100, params=params)
-
-
-pass
+    return shifted_elites
---
+++ example_pendulum.py
@@ -0,0 +1,85 @@
+import jax
+import jax.numpy as jnp
+
+import gymnasium as gym
+
+from cem import CEMParams
+from cem import sample
+from cem import update_params
+from cem import select_elites
+from cem import shift_elites
+
+rng = jax.random.PRNGKey(0)
+
+env = gym.make("Pendulum-v1", g=9.81, render_mode="human")
+proto_env = gym.make("Pendulum-v1", g=9.81)
+
+n_horizon = 30
+
+n_sequences = 20
+n_elites = 10
+n_old_elites = 5
+mean = jnp.zeros((n_horizon, env.action_space.shape[0]))
+std = jnp.ones((n_horizon, env.action_space.shape[0]))
+elites = jnp.zeros((n_elites, n_horizon, env.action_space.shape[0]))
+params = CEMParams(mean=mean, std=std, elites=elites)
+
+
+def roll_out(env, x0, action_sequences):
+    """Roll out the action sequences in the environment."""
+    n, N, _ = action_sequences.shape
+    trajectories = jnp.zeros((n, N, env.observation_space.shape[0]))
+    scores = jnp.zeros(n)
+    for i in range(n):
+        x = env.reset(seed=0)
+        env.state = x0  # Set the initial state
+        for j in range(N):
+            x, r, *_ = env.step(action_sequences[i, j])
+            trajectories = trajectories.at[i, j].set(x)
+            scores = scores.at[i].set(scores[i] + r)
+    return trajectories, scores
+
+
+n_sim_horizon = 100
+n_iters = 10
+
+x = env.reset(seed=0)
+env.render()
+
+for t in range(n_sim_horizon):
+
+    for i in range(n_iters):
+        action_sequences = sample(rng, n=n_sequences, params=params)
+        action_sequences = action_sequences.clip(
+            env.action_space.low, env.action_space.high
+        )
+
+        # Concatenate with previous elites
+        action_sequences = jnp.concatenate([action_sequences, params.elites], axis=0)
+
+        # Roll out the action sequences and get the scores
+        trajectories, scores = roll_out(proto_env, x, action_sequences)
+
+        # Select the top n elites
+        elites, elite_scores = select_elites(action_sequences, -scores, n=n_elites)
+
+        # Keep n old elites.
+        old_elites = params.elites[:n_old_elites]
+        elites = jnp.concatenate([elites, old_elites], axis=0)
+
+        # Update the parameters
+        params = update_params(elites, params=params, theta=1.0)
+
+    # Choose the best action
+    best_action = elites[0, 0]
+
+    # Simulate the environment forward one step
+    x, r, *_ = env.step(best_action)
+    env.render()
+
+    # Shift the elites
+    elites = shift_elites(rng, elites, params=params)
+    params = update_params(elites, params=params)
+
+env.close()
+proto_env.close()
